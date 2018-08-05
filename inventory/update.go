2d24d2555dbe7f8f5e76afeef9c35768f8764414
Josh Hill <jamesjoshuahill@gmail.com>
2018-08-05 18:38:15 +0100

    Refactor if statements into switch statements

--- inventory/update.go
+++ inventory/update.go
@@ -9,47 +9,47 @@ const (
 // Update changes the items in the inventory to reflect the passing of one day.
 func (i *inventory) Update() {
 	for index, item := range i.items {
-		if item.name == sulfuras {
-			continue
-		}
-
-		if item.name != agedBrie && item.name != backstagePasses {
-			if item.quality > 0 {
-				item.quality--
+		switch item.name {
+		case agedBrie:
+			if item.quality < 50 {
+				item.quality++
 			}
-		} else {
+		case backstagePasses:
 			if item.quality < 50 {
 				item.quality++
 
-				if item.name == backstagePasses {
-					if item.sellIn < 11 {
-						if item.quality < 50 {
-							item.quality++
-						}
-					}
-					if item.sellIn < 6 {
-						if item.quality < 50 {
-							item.quality++
-						}
+				if item.sellIn < 11 {
+					if item.quality < 50 {
+						item.quality++
 					}
 				}
+				if item.sellIn < 6 {
+					if item.quality < 50 {
+						item.quality++
+					}
+				}
+			}
+		case sulfuras:
+			continue
+		default:
+			if item.quality > 0 {
+				item.quality--
 			}
 		}
 
 		item.sellIn--
 
 		if item.sellIn < 0 {
-			if item.name != agedBrie {
-				if item.name != backstagePasses {
-					if item.quality > 0 {
-						item.quality--
-					}
-				} else {
-					item.quality -= item.quality
-				}
-			} else {
+			switch item.name {
+			case agedBrie:
 				if item.quality < 50 {
 					item.quality++
+				}
+			case backstagePasses:
+				item.quality = 0
+			default:
+				if item.quality > 0 {
+					item.quality--
 				}
 			}
 		}
