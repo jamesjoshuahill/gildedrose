9008e352bb675491fa3d3b8a66ffc20c0d3d4448
Josh Hill <jamesjoshuahill@gmail.com>
2018-08-05 17:57:01 +0100

    Unit tests for sell in

---
+++ inventory/update_test.go
@@ -0,0 +1,28 @@
+package inventory
+
+import (
+	. "github.com/onsi/ginkgo"
+	. "github.com/onsi/gomega"
+)
+
+var _ = Describe("Update", func() {
+	Context("when there is a standard item", func() {
+		It("reduces sell in by one", func() {
+			i := inventory{items: []Item{{sellIn: 1}}}
+
+			i.Update()
+
+			Expect(i.items[0].sellIn).To(Equal(0))
+		})
+	})
+
+	Context("when the item is Sulfuras", func() {
+		It("does not reduce sell in", func() {
+			i := inventory{items: []Item{{name: "Sulfuras, Hand of Ragnaros", sellIn: 1}}}
+
+			i.Update()
+
+			Expect(i.items[0].sellIn).To(Equal(1))
+		})
+	})
+})
