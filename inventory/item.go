ff674b94fa1f7159dc4699808cfbdb84222ecc9c
Josh Hill <jamesjoshuahill@gmail.com>
2018-08-05 17:32:12 +0100

    Not supposed to touch the Item stuct nor items var

--- inventory/item.go
+++ inventory/item.go
@@ -4,3 +4,15 @@ type Item struct {
 	name            string
 	sellIn, quality int
 }
+
+var items = []Item{
+	{name: "+5 Dexterity Vest", sellIn: 10, quality: 20},
+	{name: agedBrie, sellIn: 2, quality: 0},
+	{name: "Elixir of the Mongoose", sellIn: 5, quality: 7},
+	{name: sulfuras, sellIn: 0, quality: 80},
+	{name: sulfuras, sellIn: -1, quality: 80},
+	{name: backstagePasses, sellIn: 15, quality: 20},
+	{name: backstagePasses, sellIn: 10, quality: 49},
+	{name: backstagePasses, sellIn: 5, quality: 49},
+	{name: "Conjured Mana Cake", sellIn: 3, quality: 6},
+}
