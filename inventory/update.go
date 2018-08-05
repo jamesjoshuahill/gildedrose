8e7bac49a1dcb49b379366599fdd93a2dc2de42c
Josh Hill <jamesjoshuahill@gmail.com>
2018-08-05 18:22:35 +0100

    Use guard clause to skip sulfuras

--- inventory/update.go
+++ inventory/update.go
@@ -9,12 +9,13 @@ const (
 // Update changes the items in the inventory to reflect the passing of one day.
 func (i *inventory) Update() {
 	for n := 0; n < len(i.items); n++ {
+		if i.items[n].name == sulfuras {
+			continue
+		}
 
 		if i.items[n].name != agedBrie && i.items[n].name != backstagePasses {
 			if i.items[n].quality > 0 {
-				if i.items[n].name != sulfuras {
-					i.items[n].quality = i.items[n].quality - 1
-				}
+				i.items[n].quality = i.items[n].quality - 1
 			}
 		} else {
 			if i.items[n].quality < 50 {
@@ -34,17 +35,13 @@ func (i *inventory) Update() {
 			}
 		}
 
-		if i.items[n].name != sulfuras {
-			i.items[n].sellIn = i.items[n].sellIn - 1
-		}
+		i.items[n].sellIn -= 1
 
 		if i.items[n].sellIn < 0 {
 			if i.items[n].name != agedBrie {
 				if i.items[n].name != backstagePasses {
 					if i.items[n].quality > 0 {
-						if i.items[n].name != sulfuras {
-							i.items[n].quality = i.items[n].quality - 1
-						}
+						i.items[n].quality = i.items[n].quality - 1
 					}
 				} else {
 					i.items[n].quality -= i.items[n].quality
--- inventory/update_test.go
+++ inventory/update_test.go
@@ -24,5 +24,13 @@ var _ = Describe("Update", func() {
 
 			Expect(i.items[0].sellIn).To(Equal(1))
 		})
+
+		It("does not reduce in quality", func() {
+			i := inventory{items: []Item{{name: "Sulfuras, Hand of Ragnaros", quality: 80}}}
+
+			i.Update()
+
+			Expect(i.items[0].quality).To(Equal(80))
+		})
 	})
 })
