c9f2668cc963cf6cc17f171b326d748834b566da
Josh Hill <jamesjoshuahill@gmail.com>
2018-08-05 16:55:07 +0100

    Use constants for names of special items

--- inventory/update.go
+++ inventory/update.go
@@ -4,15 +4,21 @@ import (
 	"fmt"
 )
 
+const (
+	agedBrie        = "Aged Brie"
+	backstagePasses = "Backstage passes to a TAFKAL80ETC concert"
+	sulfuras        = "Sulfuras, Hand of Ragnaros"
+)
+
 var items = []Item{
 	{"+5 Dexterity Vest", 10, 20},
-	{"Aged Brie", 2, 0},
+	{agedBrie, 2, 0},
 	{"Elixir of the Mongoose", 5, 7},
-	{"Sulfuras, Hand of Ragnaros", 0, 80},
-	{"Sulfuras, Hand of Ragnaros", -1, 80},
-	{"Backstage passes to a TAFKAL80ETC concert", 15, 20},
-	{"Backstage passes to a TAFKAL80ETC concert", 10, 49},
-	{"Backstage passes to a TAFKAL80ETC concert", 5, 49},
+	{sulfuras, 0, 80},
+	{sulfuras, -1, 80},
+	{backstagePasses, 15, 20},
+	{backstagePasses, 10, 49},
+	{backstagePasses, 5, 49},
 	{"Conjured Mana Cake", 3, 6},
 }
 
@@ -25,16 +31,16 @@ func Print() {
 func Update() {
 	for i := 0; i < len(items); i++ {
 
-		if items[i].name != "Aged Brie" && items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+		if items[i].name != agedBrie && items[i].name != backstagePasses {
 			if items[i].quality > 0 {
-				if items[i].name != "Sulfuras, Hand of Ragnaros" {
+				if items[i].name != sulfuras {
 					items[i].quality = items[i].quality - 1
 				}
 			}
 		} else {
 			if items[i].quality < 50 {
 				items[i].quality = items[i].quality + 1
-				if items[i].name == "Backstage passes to a TAFKAL80ETC concert" {
+				if items[i].name == backstagePasses {
 					if items[i].sellIn < 11 {
 						if items[i].quality < 50 {
 							items[i].quality = items[i].quality + 1
@@ -49,15 +55,15 @@ func Update() {
 			}
 		}
 
-		if items[i].name != "Sulfuras, Hand of Ragnaros" {
+		if items[i].name != sulfuras {
 			items[i].sellIn = items[i].sellIn - 1
 		}
 
 		if items[i].sellIn < 0 {
-			if items[i].name != "Aged Brie" {
-				if items[i].name != "Backstage passes to a TAFKAL80ETC concert" {
+			if items[i].name != agedBrie {
+				if items[i].name != backstagePasses {
 					if items[i].quality > 0 {
-						if items[i].name != "Sulfuras, Hand of Ragnaros" {
+						if items[i].name != sulfuras {
 							items[i].quality = items[i].quality - 1
 						}
 					}
