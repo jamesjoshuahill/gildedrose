a08ebd14f8937349c89649aaedea0574cb62631b
Josh Hill <jamesjoshuahill@gmail.com>
2018-08-06 07:06:51 +0100

    Move List() into inventory.go

--- inventory/inventory.go
+++ inventory/inventory.go
@@ -8,3 +8,8 @@ type inventory struct {
 func New(items []Item) *inventory {
 	return &inventory{items: items}
 }
+
+// List returns a string with a line for each item including it's name, sell in, and quality.
+func (i *inventory) List() []Item {
+	return i.items
+}
--- inventory/list.go
+++
@@ -1,6 +0,0 @@
-package inventory
-
-// List returns a string with a line for each item including it's name, sell in, and quality.
-func (i *inventory) List() []Item {
-	return i.items
-}
