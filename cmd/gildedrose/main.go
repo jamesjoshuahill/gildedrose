b6ec18deeb64662460f2960ba976ab21875a13a8
Josh Hill <jhill@pivotal.io>
2018-08-09 18:40:34 +0100

    Rearrange main to match original

--- cmd/gildedrose/main.go
+++ cmd/gildedrose/main.go
@@ -6,25 +6,25 @@ import (
 	"github.com/jamesjoshuahill/gildedrose"
 )
 
-var items = []gildedrose.Item{
-	{Name: "+5 Dexterity Vest", SellIn: 10, Quality: 20},
-	{Name: "Aged Brie", SellIn: 2, Quality: 0},
-	{Name: "Elixir of the Mongoose", SellIn: 5, Quality: 7},
-	{Name: "Sulfuras, Hand of Ragnaros", SellIn: 0, Quality: 80},
-	{Name: "Sulfuras, Hand of Ragnaros", SellIn: -1, Quality: 80},
-	{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 15, Quality: 20},
-	{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 10, Quality: 49},
-	{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 5, Quality: 49},
-	{Name: "Conjured Mana Cake", SellIn: 3, Quality: 10},
-}
-
 func main() {
+	fmt.Println("OMGHAI!")
+
+	items := []gildedrose.Item{
+		{Name: "+5 Dexterity Vest", SellIn: 10, Quality: 20},
+		{Name: "Aged Brie", SellIn: 2, Quality: 0},
+		{Name: "Elixir of the Mongoose", SellIn: 5, Quality: 7},
+		{Name: "Sulfuras, Hand of Ragnaros", SellIn: 0, Quality: 80},
+		{Name: "Sulfuras, Hand of Ragnaros", SellIn: -1, Quality: 80},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 15, Quality: 20},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 10, Quality: 49},
+		{Name: "Backstage passes to a TAFKAL80ETC concert", SellIn: 5, Quality: 49},
+		{Name: "Conjured Mana Cake", SellIn: 3, Quality: 10},
+	}
+
 	app := gildedrose.New(items)
 
 	for day := 0; day <= 30; day++ {
-		if day == 0 {
-			fmt.Println("OMGHAI!")
-		} else {
+		if day != 0 {
 			fmt.Println()
 		}
 		fmt.Printf("-------- day %d --------\n", day)
