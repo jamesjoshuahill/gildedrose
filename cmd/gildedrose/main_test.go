5c6ab62fd91470997e89cd3a4ab039e81fd52c47
Josh Hill <jhill@pivotal.io>
2018-08-16 17:37:44 +0100

    Simplify example integration test for conjured items

--- cmd/gildedrose/main_test.go
+++ cmd/gildedrose/main_test.go
@@ -14,16 +14,7 @@ import (
 var _ = Describe("Gilded Rose", func() {
 	It("updates all items correctly", func() {
 		expectedOutput := readTestData("golden_master.txt")
-
-		session, err := gexec.Start(exec.Command(binaryPath), GinkgoWriter, GinkgoWriter)
-
-		Expect(err).NotTo(HaveOccurred())
-		Eventually(session).Should(gexec.Exit(0))
-		Expect(string(session.Out.Contents())).To(Equal(expectedOutput))
-	})
-
-	PIt("updates all items correctly including conjured", func() {
-		expectedOutput := readTestData("golden_master_with_conjured_item.txt")
+		// expectedOutput := readTestData("golden_master_with_conjured_item.txt")
 
 		session, err := gexec.Start(exec.Command(binaryPath), GinkgoWriter, GinkgoWriter)
 
