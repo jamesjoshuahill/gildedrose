c95ec9bf3170acdd8331cc55a88a60d9f7d941a8
Josh Hill <jhill@pivotal.io>
2018-08-28 17:36:00 +0100

    Group backstage passes logic

--- app.go
+++ app.go
@@ -28,37 +28,41 @@ func (g *App) UpdateQuality() {
 			continue
 		}
 
-		if g.Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-			if g.Items[i].Quality > 0 {
-				g.Items[i].Quality = g.Items[i].Quality - 1
-			}
-		} else {
+		if g.Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
 			if g.Items[i].Quality < 50 {
 				g.Items[i].Quality = g.Items[i].Quality + 1
-				if g.Items[i].Name == "Backstage passes to a TAFKAL80ETC concert" {
-					if g.Items[i].SellIn < 11 {
-						if g.Items[i].Quality < 50 {
-							g.Items[i].Quality = g.Items[i].Quality + 1
-						}
+
+				if g.Items[i].SellIn < 11 {
+					if g.Items[i].Quality < 50 {
+						g.Items[i].Quality = g.Items[i].Quality + 1
 					}
-					if g.Items[i].SellIn < 6 {
-						if g.Items[i].Quality < 50 {
-							g.Items[i].Quality = g.Items[i].Quality + 1
-						}
+				}
+
+				if g.Items[i].SellIn < 6 {
+					if g.Items[i].Quality < 50 {
+						g.Items[i].Quality = g.Items[i].Quality + 1
 					}
 				}
 			}
+
+			g.Items[i].SellIn = g.Items[i].SellIn - 1
+
+			if g.Items[i].SellIn < 0 {
+				g.Items[i].Quality = g.Items[i].Quality - g.Items[i].Quality
+			}
+
+			continue
+		}
+
+		if g.Items[i].Quality > 0 {
+			g.Items[i].Quality = g.Items[i].Quality - 1
 		}
 
 		g.Items[i].SellIn = g.Items[i].SellIn - 1
 
 		if g.Items[i].SellIn < 0 {
-			if g.Items[i].Name != "Backstage passes to a TAFKAL80ETC concert" {
-				if g.Items[i].Quality > 0 {
-					g.Items[i].Quality = g.Items[i].Quality - 1
-				}
-			} else {
-				g.Items[i].Quality = g.Items[i].Quality - g.Items[i].Quality
+			if g.Items[i].Quality > 0 {
+				g.Items[i].Quality = g.Items[i].Quality - 1
 			}
 		}
 	}
